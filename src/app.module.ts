ed3204d5e57a43be956f114dc1a06d0814e6c982
Ilya Kozlovski <kozlvskiilya@gmail.com>
2025-06-24 19:10:52 +0300

    add TestingModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoreModule } from './core/core.module';
 import { BloggerPlatformModule } from './modules/bloggers-platform/blogger-platform.module';
+import { TestingModule } from './modules/testing/testing.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-bloggers-platform'),
     CoreModule,
     BloggerPlatformModule,
+    TestingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
