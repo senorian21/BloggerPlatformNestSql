3b1489649ff22af49b611c7bbc13527d164b0ec4
Ilya Kozlovski <kozlvskiilya@gmail.com>
2025-07-16 11:01:05 +0300

    add config

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSetup } from './setup/app.setup';
 import cookieParser from 'cookie-parser';
+import { CoreConfig } from './core/core.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+  const coreConfig = app.get<CoreConfig>(CoreConfig);
+
   appSetup(app);
+
+  const port = coreConfig.port;
+
   app.enableCors();
-  await app.listen(process.env.PORT ?? 5003);
+
+  await app.listen(port, () => {
+    console.log('App starting listen port: ', port);
+    console.log('NODE_ENV: ', coreConfig.env);
+  });
 }
 bootstrap();
